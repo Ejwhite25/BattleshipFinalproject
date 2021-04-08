27e3504273b92872f6f8c181d1e9c5162130ea69
ordonez99 <marcos.ordonez@western.edu>
2021-04-08 14:28:37 -0600

    Player and Thread

---
+++ src/Thread.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyThread {
+    Runnable job;
+    Thread thread;
+    Socket connection;
+    InputStreamReader reader;
+    PrintWriter writer;
+    ServerSocket server;
+
+    public static void main(String[] args){
+        new MyThread().go();
+    }
+    private void go(){
+        try {
+            server = new ServerSocket(5000);
+            while(true) {
+                connection = server.accept();
+
+                job = new Job(connection);
+                thread = new Thread(job);
+                thread.start();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    class Job implements Runnable {
+        Socket socket;
+
+        Job(Socket name) {
+            socket = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                writer = new PrintWriter(connection.getOutputStream());
+                writer.println("connected to the server");
+                writer.flush();
+
+                reader = new InputStreamReader(connection.getInputStream());
+                BufferedReader buffer = new BufferedReader(reader);
+                String msg = buffer.readLine();
+                System.out.println(msg);
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
