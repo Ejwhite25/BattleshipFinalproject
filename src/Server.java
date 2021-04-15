f81be66156834e63d42b1a6e4b954d7fb1526151
Ordonez99 <marcos.ordonez@western.edu>
2021-04-15 15:17:07 -0600

    thread working

--- src/Server.java
+++ src/Server.java
@@ -9,14 +9,11 @@ import java.net.Socket;
 
 public class Server {
     public static int playerID = 0;
-    // player 1 is 1.
-    //player 2 is 2.
     private Object lock= new Object();
     Runnable jobOne;
     Runnable jobTwo;
     Thread threadOne;
     Thread threadTwo;
-    Socket connection;
     private BufferedReader reader;
     PrintWriter writer;
     ServerSocket server;
@@ -43,83 +40,25 @@ public class Server {
         new Server().go();
     }
     private void go(){
+
         try {
             server = new ServerSocket(9000);
-            while(true) {
-                connection = server.accept();
+            Socket connection = server.accept();
+
                 jobOne = new JobOne(connection);
                 threadOne = new Thread(jobOne);
                 threadOne.start();
-
-                jobTwo = new JobTwo(connection);
+                Socket connectionTwo = server.accept();
+                jobTwo = new JobTwo(connectionTwo);
                 threadTwo = new Thread(jobOne);
                 threadTwo.start();
 
-            }
-        } catch(IOException e){
-            e.printStackTrace();
+            } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
     }
 
-    /*class Job implements Runnable {
-        Socket socket;
 
-        Job(Socket name) {
-            socket = name;
-        }
-
-        @Override
-        public void run() {
-            try {
-                writer = new PrintWriter(connection.getOutputStream());
-                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                while(socket.isConnected()) {
-                    if (playerID == 0) {
-                        playerID = 1;
-                    }
-                    if (playerID == 1) {
-                        writer.println("Player 1's turn");
-                        writer.flush();
-
-                    } else if (playerID == 2) {
-                        writer.println("PLayer 2's turn");
-                        writer.flush();
-                    }
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        String randomWord = "Dog";
-                        if (line == randomWord) {
-                            writer.println("Player" + playerID + "s guess was correct!");
-                            writer.flush();
-                            if (playerID == 1) {
-                                playerID = 2;
-                            } else {
-                                playerID = 1;
-                            }
-                            writer.println("Player" + playerID + "turn");
-
-                        } else {
-                            writer.println("Player" + playerID + "s guess was incorrect!");
-                            if (playerID == 1) {
-                                playerID = 2;
-                            } else {
-                                playerID = 1;
-                            }
-                            writer.println("Player" + playerID + "turn");
-                            writer.flush();
-
-
-                        }
-                    }
-
-
-                    }
-
-        } catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-    }*/
     class JobOne implements Runnable {
         Socket socket;
 
@@ -136,6 +75,8 @@ public class Server {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                notifyLock();
+                waitForLock();
             }
         }
     }
@@ -150,6 +91,7 @@ public class Server {
             @Override
             public void run() {
                 while (true) {
+                    waitForLock();
                     System.out.println("First player: ");
                     System.out.println("First player: ");
                     try {
@@ -157,6 +99,7 @@ public class Server {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    notifyLock();
                 }
             }
         }
