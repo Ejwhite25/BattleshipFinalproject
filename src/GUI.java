bae7a36cee0c0be65d69b1c6ad16243bb725e1f2
Ordonez99 <marcos.ordonez@western.edu>
2021-04-20 14:27:23 -0600

    server and players are interactive.

---
+++ src/GUI.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class GUI {
+    JFrame f;
+    JFrame frame;
+    JLabel label;
+    ArrayList<JButton>buttons;
+    public void GUI() {
+        label = new JLabel();
+        f = new JFrame();
+        frame = new JFrame();
+        buttons = new ArrayList<>();
+    }
+    //  f.getContentPane().add(BorderLayout.NORTH,label);
+    public void makeBoard(){
+
+    }
+
+}
